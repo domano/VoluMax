5c343eaa12f5a9259afb2a9192c51340cf4a017c
Dino Omanovic <dino.omanovic@icloud.com>
2025-05-09 20:08:41 +0200

    Initial Commit

---
+++ VoluMax/VoluMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoluMaxApp.swift
+//  VoluMax
+//
+//  Created by Dino Omanovic on 09.05.25.
+//
+
+import SwiftUI
+
+@main
+struct VoluMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
